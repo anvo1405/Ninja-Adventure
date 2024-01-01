91c063dc52c38a6b727f63bfbcd203df6b9b325c
NhatMinh <minhphan113@gmail.com>
2024-01-01 20:55:44 +0700

    feat: create moving animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,35 @@ public class PlayerController : MonoBehaviour
     Vector2 moveInput;
 
     public float walkSpeed = 5f;
+    public float runSpeed = 8f;
 
+    public float CurrentMoveSpeed
+    {
+        get { 
+            if(IsMoving)
+            {
+                if(IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            } else { return 0; }
+            
+        }
+    }
+
+    [SerializeField]
     private bool _isMoving = false;
 
+    [SerializeField]
     private bool _isRunning = false;
 
     public bool IsMoving { get
         {
-            return _isMoving
+            return _isMoving;
         } private set {
             _isMoving = value;
             animator.SetBool("isMoving", value);
@@ -27,7 +48,7 @@ public class PlayerController : MonoBehaviour
     public bool IsRunning { 
         get
             {
-                return _isRunning
+            return _isRunning;
             }
         private set
         {
@@ -35,6 +56,19 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("isRunning", value);
         }
     }
+
+    public bool IsFacingRight { get { return _isFacingRight; } private set { 
+            if(_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1,1);
+
+            }
+                
+            _isFacingRight = value;
+        
+        } }
+
+    public bool _isFacingRight = true;
 
     Rigidbody2D rb;
     Animator animator;
@@ -47,7 +81,20 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed * Time.fixedDeltaTime * 100, rb.velocity.y);
+
+
+    }
+
+    public void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        } else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight= false;
+        }
     }
 
     private void Awake()
@@ -73,6 +120,17 @@ public class PlayerController : MonoBehaviour
 
        IsMoving = moveInput != Vector2.zero;
 
-        
+        SetFacingDirection(moveInput);
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        } else if(context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
