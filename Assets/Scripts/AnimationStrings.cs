09d95e275e2433850ad770f4fb42a6a4fede48c5
anvo1405 <vohoaian62@gmail.com>
2024-01-05 20:43:18 +0700

    Jumping and Air Movement

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 internal class AnimationStrings : MonoBehaviour
 {
     internal static string isMoving = "isMoving";
     internal static string isRuning = "isRuning";
+    internal static string IsGrounded = "isGrounded";
+    internal static string yVelocity = "yVelocity";
+    internal static string jump = "jump";
+    internal static string isOnWall = "isOnWall";
+    internal static string isOnCeiling = "isOnCeiling";
 }
